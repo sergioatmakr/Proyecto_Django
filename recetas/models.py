c49560b9341f65194921bd41c7e023d8048819ed
sergioarmakr <scanseco.tapia@gmail.com>
2026-05-22 07:34:53 +0200

    implementar modelo Receta con relacion ForeignKey a User y fechas

--- recetas/models.py
+++ recetas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Receta(models.Model):
+    titulo = models.CharField(max_length=200, verbose_name="Título de la receta")
+    descripcion = models.TextField(verbose_name="Descripción corta/Resumen")
+    ingredientes = models.TextField(help_text="Escribe los ingredientes separados por líneas.")
+    instrucciones = models.TextField(verbose_name="Pasos de preparación")
+    fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de publicación")
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recetas')
+
+    def __str__(self):
+        return self.titulo
