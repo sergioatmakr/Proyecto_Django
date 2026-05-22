8a5b25db419ade2112d2ee9c3e4a27ed3c61024e
sergioarmakr <scanseco.tapia@gmail.com>
2026-05-22 07:40:06 +0200

    panel de administrador

--- recetas/admin.py
+++ recetas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Receta
 
-# Register your models here.
+@admin.register(Receta)
+class RecetaAdmin(admin.ModelAdmin):
+    # Esto hará que en el panel veas una bonita tabla con el título, autor y fecha de cada receta
+    list_display = ('titulo', 'autor', 'fecha_creacion')
+    # Añade un buscador rápido en el panel de administración
+    search_fields = ('titulo', 'descripcion')
